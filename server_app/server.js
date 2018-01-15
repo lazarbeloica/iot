cb6e677623507b8b44ba6efb0eb8bf79a0992629
Lazar Beloica <Lazar.Beloica@rt-rk.com>
2018-01-15 17:20:17 +0100

    SERVER: Server can be deployed now

--- server_app/server.js
+++ server_app/server.js
@@ -7,10 +7,38 @@ port = process.env.PORT || 3000,
 mongoose = require('mongoose'),
 Task = require('./api/models/serverModel'), //created model loading here
 bodyParser = require('body-parser');
+var DbCredentials = {
+  schema: "mongodb://",
+  user: "beloica",
+  pass: "Aa12345678",
+  host: "ds251737.mlab.com",
+  port: 51737,
+  db : "checkmytmp"
+};
+
+var URI = DbCredentials.schema +
+          DbCredentials.user +
+          ":" +
+          DbCredentials.pass +
+          "@"
+          + DbCredentials.host
+          + ":"
+          + DbCredentials.port
+          + "/"
+          + DbCredentials.db;
+console.log(URI);
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.connect(URI);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
+  console.log("Connection open!");
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,4 +52,4 @@ app.use(function(req, res) {
 
 app.listen(port);
 
-console.log('Lazars measurements server started on: ' + port);
+console.log('Lazar\'s measurements server started on: ' + port);
