be0d8811bbd78d1da03d4e2f39477351400c873d
Lazar Beloica <lazarbeloica@gmail.com>
2018-03-23 00:41:39 +0100

    CLIENTSIDE: Created a page to dislay device measurements

--- server_app/api/routes/serverRoutes.js
+++ server_app/api/routes/serverRoutes.js
@@ -1,17 +1,22 @@
 
 
 'use strict';
-module.exports = function(app) {
+module.exports = function (app) {
   var todoList = require('../controllers/serverController');
 
   // todoList Routes
+  app.route('/devices')
+    .get(todoList.list_all_devices);
+
   app.route('/measurements')
     .get(todoList.list_all_measurements)
     .post(todoList.create_a_measurement);
-
 
   app.route('/measurements/:measurementId')
     .get(todoList.read_a_measurement)
     .put(todoList.update_a_measurement)
     .delete(todoList.delete_a_measurement);
-};  
+
+  app.route('/devsmeasurements/:devId')
+    .get(todoList.read_devices_measurements);
+};
--- server_app/server.js
+++ server_app/server.js
@@ -30,7 +30,7 @@ console.log(URI);
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect(URI);
+mongoose.connect('mongodb://localhost/Tododb');
 
 var db = mongoose.connection;
 
@@ -42,6 +42,25 @@ db.once('open', function callback () {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 var routes = require('./api/routes/serverRoutes'); //importing route
 routes(app); //register the route
